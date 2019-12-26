d81c94884572b6ab13cbb0e323f3eacb59af3631
Vadim Kim <17victork@gmail.com>
2019-12-25 23:10:02 -0800

    hitboxes, ai, lazers
    got a bunch of stuff working, first of all, lazers are now functional and thing correctly identify when they are hit, another thing is that hitboxes can now be shown, I made it so you can controll one of the ships too, ai is better now too, rotating around each eaother to look more natural as trafic

--- newSketch.js
+++ newSketch.js
@@ -1,28 +1,42 @@
 var ships = [];
+var mainBossShip;
 var asteroids = [];
 var screenGrid;
 var lazerGrid;
+
 var globalIDCounter = 0;
-var maxDetectionLength = 70;
+var lazerGlobalIDCounter = 0;
+
+var maxDetectionLength = 30;
+var screenGridScale = 70;
+var hitDetectionLength = 2;
+var lazerGridScale = 70;
+
 var lazers = [];
+
+var shipSpeed = {
+  min: 0.05,
+  max: 0.2
+};
 
 function setup() {
   /* creating a canvas and attatching it to a div */
   let myCanvas = createCanvas(innerWidth, innerHeight);
   myCanvas.parent("mainSketch");
 
-  screenGrid = new Grid(maxDetectionLength);
+  screenGrid = new Grid(screenGridScale);
   screenGrid.populate(innerWidth, innerHeight);
-  lazerGrid = new Grid(maxDetectionLength);
+  lazerGrid = new Grid(lazerGridScale);
   lazerGrid.populate(innerWidth, innerHeight);
 
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 2; i++) {
     ships.push(
       new ship(
         random(0, innerWidth),
         random(0, innerHeight),
         random(0, 360),
-        random(0.05, 0.2),
+        random(3, 6),
+        random(shipSpeed.min, shipSpeed.max),
         random(1, 5),
         100,
         10,
@@ -33,22 +47,30 @@ function setup() {
       )
     );
   }
+  mainBossShip = new ship(
+    innerWidth / 2,
+    innerHeight / 2,
+    0,
+    20,
+    0.35,
+    2,
+    1000,
+    30,
+    screenGrid,
+    lazerGrid,
+    [],
+    lazers
+  );
   //   lazer = new Lazer(100, 100, 20, 3);
   // ships.push(new ship(300, 300, 0, 0.1, 10, screenGrid, ships));
-  background(0);
+  // background(0);
 }
 
 function draw() {
   background(0);
   screenGrid.clean();
-  for (let i = 0; i < ships.length; i++) {
-    ships[i].show();
-    ships[i].run();
-    ships[i].addVel();
-    ships[i].checkGrid();
-    ships[i].swarmWithOtherShips();
-    // ships[i].rotation++;
-  }
+  lazerGrid.clean();
+  mainBossShip.showHealthBar();
   for (let i = 0; i < lazers.length; i++) {
     if (lazers[i].delete == true) {
       lazers.splice(i, 1);
@@ -56,17 +78,47 @@ function draw() {
     } else {
       lazers[i].show();
       lazers[i].run();
+      lazers[i].checkGrid();
     }
   }
-  debugGrid();
-  //   lazer.show();
-  //   lazer.run();
+  for (let i = 0; i < ships.length; i++) {
+    ships[i].show();
+    ships[i].showHealthBar();
+    ships[i].run();
+    // ships[i].addVel();
+    ships[i].checkGrid();
+    ships[i].swarmWithOtherShips();
+    ships[i].checkIfShot();
+    fill(200, 0, 0, 100);
+    circle(ships[i].x, ships[i].y, hitDetectionLength * ships[i].size);
+    // ships[i].rotation++;
+  }
+  mainBossShip.show();
+  if (frameCount % 20 == 1) {
+  }
+  // lazerDetectionDebugGrid();
+  shipDetectionDebugGrid();
+  if (keyIsDown(LEFT_ARROW)) {
+    ships[0].addRotatio(-3);
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    ships[0].addRotatio(3);
+  }
+  if (keyIsDown(UP_ARROW)) {
+    ships[0].addVel();
+  }
 }
 
-function debugGrid() {
+function keyPressed() {
+  if (keyCode == 32) {
+    ships[0].shoot();
+  }
+}
+
+function shipDetectionDebugGrid() {
   noFill();
   strokeWeight(1);
-  stroke(200, 50, 200);
+  stroke(200, 50, 200, 100);
   for (let j = 0; j < innerHeight / screenGrid.scale; j++) {
     for (let i = 0; i < innerWidth / screenGrid.scale; i++) {
       rect(
@@ -74,6 +126,21 @@ function debugGrid() {
         j * screenGrid.scale,
         screenGrid.scale,
         screenGrid.scale
+      );
+    }
+  }
+}
+function lazerDetectionDebugGrid() {
+  noFill();
+  strokeWeight(1);
+  stroke(150, 50, 0, 100);
+  for (let j = 0; j < innerHeight / lazerGrid.scale; j++) {
+    for (let i = 0; i < innerWidth / lazerGrid.scale; i++) {
+      rect(
+        i * lazerGrid.scale,
+        j * lazerGrid.scale,
+        lazerGrid.scale,
+        lazerGrid.scale
       );
     }
   }
@@ -128,6 +195,9 @@ class Grid {
           this.removeQueue[i].id
         ) {
           this.grid[this.removeQueue[i].y][this.removeQueue[i].x].splice(j, 1);
+          // console.log(
+          //   `removed at ${this.removeQueue[i].x}, ${this.removeQueue[i].y}`
+          // );
         }
       }
     }
@@ -135,11 +205,12 @@ class Grid {
   }
 }
 
-class ship {
+class bossShip {
   constructor(
     x,
     y,
     rotation,
+    size,
     speed,
     rotationSpeed,
     maxHealth,
@@ -154,7 +225,7 @@ class ship {
     this.velx = 0;
     this.vely = 0;
     this.rotation = rotation; /* Ship's rotation from the horizontal */
-    this.size = 5; /* Just how big the ship is, there is no particular scale and it is more or less arbitrary*/
+    this.size = size; /* Just how big the ship is, there is no particular scale and it is more or less arbitrary*/
     this.friction = 0.95;
     this.grid = grid;
     this.gridX = Math.floor(this.x / this.grid.scale);
@@ -167,8 +238,150 @@ class ship {
     this.speed = speed;
     this.rotationSpeed = rotationSpeed;
     this.maxHealth = maxHealth;
+    this.currentHealth = maxHealth;
     this.AttackStrength = AttackStrength;
     this.lazersArray = lazersArray;
+    this.opacityOfHealth = 0;
+  }
+
+  show() {
+    /* 
+      points to draw the ship itself (angle, magnitude)
+      (0, size)
+      (100, size)
+      (180, size/2)
+      (260, size)
+      (0, size)
+    */
+    strokeWeight(1);
+    stroke(255);
+    noFill();
+    beginShape();
+    vertex(
+      this.x + this.size * 1.5 * Math.cos((this.rotation * PI) / 180),
+      this.y + this.size * 1.5 * Math.sin((this.rotation * PI) / 180)
+    );
+    vertex(
+      this.x + this.size * Math.cos(((this.rotation + 140) * PI) / 180),
+      this.y + this.size * Math.sin(((this.rotation + 140) * PI) / 180)
+    );
+    vertex(
+      this.x + (this.size / 2) * Math.cos(((this.rotation + 180) * PI) / 180),
+      this.y + (this.size / 2) * Math.sin(((this.rotation + 180) * PI) / 180)
+    );
+    vertex(
+      this.x + this.size * Math.cos(((this.rotation + 220) * PI) / 180),
+      this.y + this.size * Math.sin(((this.rotation + 220) * PI) / 180)
+    );
+    vertex(
+      this.x + this.size * 1.5 * Math.cos((this.rotation * PI) / 180),
+      this.y + this.size * 1.5 * Math.sin((this.rotation * PI) / 180)
+    );
+
+    endShape();
+    if (this.opacityOfHealth > 0) {
+      this.opacityOfHealth -= 2;
+    }
+  }
+
+  hit(lazer) {
+    console.log(
+      `Ship ID${this.ID} has been hit by lazer from ${lazer.senderID}`
+    );
+    this.opacityOfHealth = 255;
+  }
+
+  showHealthBar() {
+    let pixelsPerPoint = 0.0001;
+    let healthBarX = this.x - (this.maxHealth / 2) * pixelsPerPoint;
+    let healthBarY = this.y + this.size * 3;
+    let healthBarW = this.maxHealth * pixelsPerPoint;
+    let healthBarH = this.size / 2;
+    fill(159, 159, 159, this.opacityOfHealth);
+    stroke(159, 159, 159, this.opacityOfHealth);
+    rect(healthBarX, healthBarY, healthBarW, healthBarH);
+    fill(255, 0, 0, this.opacityOfHealth);
+    rect(healthBarX, healthBarY, healthBarW, healthBarH);
+  }
+
+  run() {
+    /* adds velocity to its x and y positions, and then reduces vel depending on the value */
+    this.x += this.velx;
+    this.y += this.vely;
+    this.velx *= this.friction;
+    this.vely *= this.friction;
+    if (this.x < 0) {
+      this.x = innerWidth;
+    } else if (this.x > innerWidth) {
+      this.x = 0;
+    }
+    if (this.y < 0) {
+      this.y = innerHeight;
+    } else if (this.y > innerHeight) {
+      this.y = 0;
+    }
+  }
+
+  addRotatio(magnitude) {
+    this.rotation += magnitude;
+  }
+
+  addVel() {
+    this.velx += this.speed * Math.cos((this.rotation * PI) / 180);
+    this.vely += this.speed * Math.sin((this.rotation * PI) / 180);
+  }
+
+  shoot() {
+    /*
+      The idea is that you just send a lazer forward, the problem is to make sure it doesn't crash 
+      into it instantly so you need to somehow place it in fron of the ship, and at speed higher than, 
+      of the same as the ship
+    */
+    let onGridX = Math.floor(this.x / this.lazerGrid.scale);
+    let onGridY = Math.floor(this.y / this.lazerGrid.scale);
+    let newLazer = new Lazer(this.x, this.y, this.rotation, 5);
+    this.lazerGrid.addItemTo(onGridX, onGridY, newLazer, this.lazerGrid);
+    this.lazersArray.push(newLazer);
+  }
+}
+
+class ship {
+  constructor(
+    x,
+    y,
+    rotation,
+    size,
+    speed,
+    rotationSpeed,
+    maxHealth,
+    AttackStrength,
+    grid,
+    lazerGrid,
+    shipsArray,
+    lazersArray
+  ) {
+    this.x = x;
+    this.y = y;
+    this.velx = 0;
+    this.vely = 0;
+    this.rotation = rotation; /* Ship's rotation from the horizontal */
+    this.size = size; /* Just how big the ship is, there is no particular scale and it is more or less arbitrary*/
+    this.friction = 0.95;
+    this.grid = grid;
+    this.gridX = Math.floor(this.x / this.grid.scale);
+    this.gridY = Math.floor(this.y / this.grid.scale);
+    this.grid.addItemTo(this.gridX, this.gridY, this);
+    this.lazerGrid = lazerGrid;
+    this.ID = globalIDCounter;
+    globalIDCounter++;
+    this.shipsArray = shipsArray;
+    this.speed = speed;
+    this.rotationSpeed = rotationSpeed;
+    this.maxHealth = maxHealth;
+    this.currentHealth = maxHealth;
+    this.attackStrength = AttackStrength;
+    this.lazersArray = lazersArray;
+    this.opacityOfHealth = 0;
   }
   show() {
     /* 
@@ -205,6 +418,120 @@ class ship {
     );
 
     endShape();
+
+    if (this.opacityOfHealth > 0) {
+      this.opacityOfHealth -= 2;
+    }
+  }
+
+  checkIfShot() {
+    /*
+    Very much the same thing as swarming with others, except 
+    you perform it on the lazer grid and perform a different function
+    when one is close enough
+    */
+    let surroundingLazers = [];
+    surroundingLazers = surroundingLazers.concat(
+      this.lazerGrid.grid[this.gridY][this.gridX]
+    );
+    if (this.gridX > 0) {
+      //   console.log(this.grid.grid[this.gridY][this.gridX - 1]);
+      surroundingLazers = surroundingLazers.concat(
+        this.lazerGrid.grid[this.gridY][this.gridX - 1]
+      );
+      if (this.gridY > 0) {
+        surroundingLazers = surroundingLazers.concat(
+          this.lazerGrid.grid[this.gridY - 1][this.gridX - 1]
+        );
+      }
+      if (this.gridY < this.lazerGrid.length) {
+        surroundingLazers = surroundingLazers.concat(
+          this.lazerGrid.grid[this.gridY + 1][this.gridX - 1]
+        );
+      }
+    }
+    if (this.gridX < this.lazerGrid.grid[0].length) {
+      surroundingLazers = surroundingLazers.concat(
+        this.lazerGrid.grid[this.gridY][this.gridX + 1]
+      );
+      if (this.gridY > 0) {
+        surroundingLazers = surroundingLazers.concat(
+          this.lazerGrid.grid[this.gridY - 1][this.gridX + 1]
+        );
+      }
+      if (this.gridY < this.lazerGrid.length) {
+        surroundingLazers = surroundingLazers.concat(
+          this.lazerGrid.grid[this.gridY + 1][this.gridX + 1]
+        );
+      }
+    }
+    if (this.gridY > 0) {
+      surroundingLazers = surroundingLazers.concat(
+        this.lazerGrid.grid[this.gridY - 1][this.gridX]
+      );
+    }
+    if (this.gridY < this.lazerGrid.length) {
+      surroundingLazers = surroundingLazers.concat(
+        this.lazerGrid.grid[this.gridY + 1][this.gridX]
+      );
+    }
+
+    if (surroundingLazers.length < 1) {
+      return;
+    }
+    for (let i = 0; i < surroundingLazers.length; i++) {
+      if (surroundingLazers[i] == undefined) {
+      } else if (
+        Math.sqrt(
+          Math.pow(this.x - surroundingLazers[i].x, 2) +
+            Math.pow(this.y - surroundingLazers[i].y, 2)
+        ) <
+        hitDetectionLength * this.size
+      ) {
+        /*
+         basically run this if the lazer is withing your surrounding grids 
+         and the distance to it is small enough 
+         */
+
+        // console.log(
+        //   Math.sqrt(
+        //     (Math.pow(this.x - surroundingLazers[i].x, 2) +
+        //       Math.pow(this.y - surroundingLazers[i].y),
+        //     2)
+        //   )
+        // );
+        if (surroundingLazers[i].senderID != this.ID) {
+          this.hit(surroundingLazers[i]);
+          surroundingLazers[i].lazerGrid.removeQueue.push({
+            x: surroundingLazers[i].gridX,
+            y: surroundingLazers[i].gridY,
+            id: surroundingLazers[i].ID
+          });
+          this.lazersArray.splice(i, 1);
+          return;
+        }
+      }
+    }
+  }
+
+  hit(lazer) {
+    console.log(
+      `Ship ID${this.ID} has been hit by lazer from ID${lazer.senderID}`
+    );
+    this.opacityOfHealth = 255;
+  }
+
+  showHealthBar() {
+    let pixelsPerPoint = 0.2;
+    let healthBarX = this.x - (this.maxHealth / 2) * pixelsPerPoint;
+    let healthBarY = this.y + this.size * 3;
+    let healthBarW = this.maxHealth * pixelsPerPoint;
+    let healthBarH = this.size / 2;
+    fill(159, 159, 159, this.opacityOfHealth);
+    stroke(159, 159, 159, this.opacityOfHealth);
+    rect(healthBarX, healthBarY, healthBarW, healthBarH);
+    fill(255, 0, 0, this.opacityOfHealth);
+    rect(healthBarX, healthBarY, healthBarW, healthBarH);
   }
 
   swarmWithOtherShips() {
@@ -261,17 +588,16 @@ class ship {
     }
 
     // console.log(surroundingShips);
-    if (surroundingShips.length < 0) {
+    if (surroundingShips.length < 1) {
       return;
     }
     for (let i = 0; i < surroundingShips.length; i++) {
       if (surroundingShips[i] == undefined) {
       } else if (
-        ((this.x - surroundingShips[i].x) ^
-          (2 + (this.y - surroundingShips[i].y)) ^
-          2 ^
-          0.5) <
-        maxDetectionLength
+        Math.sqrt(
+          Math.pow(this.x - surroundingShips[i].x, 2) +
+            Math.pow(this.y - surroundingShips[i].y, 2)
+        ) < maxDetectionLength
       ) {
         // console.log("ree");
         let deltaRotation = Math.sign(
@@ -317,7 +643,16 @@ class ship {
     */
     let onGridX = Math.floor(this.x / this.lazerGrid.scale);
     let onGridY = Math.floor(this.y / this.lazerGrid.scale);
-    let newLazer = new Lazer(this.x, this.y, this.rotation, 3);
+    let newLazer = new Lazer(
+      this.x,
+      this.y,
+      this.rotation,
+      5,
+      this.attackStrength,
+      this.ID,
+      this.lazersArray,
+      this.lazerGrid
+    );
     this.lazerGrid.addItemTo(onGridX, onGridY, newLazer);
     this.lazersArray.push(newLazer);
   }
@@ -338,7 +673,7 @@ class ship {
 }
 
 class Lazer {
-  constructor(x, y, angle, speed) {
+  constructor(x, y, angle, speed, damage, senderID, lazersArray, lazerGrid) {
     this.x = x;
     this.y = y;
     this.angle = angle;
@@ -346,6 +681,14 @@ class Lazer {
     this.vx = this.speed * Math.cos((angle * PI) / 180);
     this.vy = this.speed * Math.sin((angle * PI) / 180);
     this.delete = false;
+    this.damage = damage;
+    this.senderID = senderID;
+    this.lazersArray = lazersArray;
+    this.lazerGrid = lazerGrid;
+    this.gridX = Math.floor(this.x / this.lazerGrid.scale);
+    this.gridY = Math.floor(this.y / this.lazerGrid.scale);
+    this.ID = lazerGlobalIDCounter;
+    lazerGlobalIDCounter++;
   }
 
   show() {
@@ -364,15 +707,39 @@ class Lazer {
   }
 
   run() {
-    this.x += this.vx;
-    this.y += this.vy;
     if (
-      this.x > innerWidth ||
-      this.x < 0 ||
-      this.y > innerHeight ||
-      this.y < 0
+      this.x + this.vx < 0 ||
+      this.x + this.vx > innerWidth ||
+      this.y + this.vy < 0 ||
+      this.y + this.vy > innerHeight
     ) {
       this.delete = true;
+      this.lazerGrid.removeQueue.push({
+        x: this.gridX,
+        y: this.gridY,
+        id: this.senderID
+      });
+    } else {
+      this.x += this.vx;
+      this.y += this.vy;
+    }
+  }
+
+  checkGrid() {
+    /* checks if it is still in the same place on the grid, if not, moves it to the other place */
+    let newGridX = Math.floor(this.x / this.lazerGrid.scale);
+    let newGridY = Math.floor(this.y / this.lazerGrid.scale);
+    if (newGridX != this.gridX || newGridY != this.gridY) {
+      this.lazerGrid.addItemTo(newGridX, newGridY, this);
+      //   console.log(this.grid.grid);
+      this.lazerGrid.removeQueue.push({
+        x: this.gridX,
+        y: this.gridY,
+        id: this.ID
+      });
+      this.gridX = newGridX;
+      this.gridY = newGridY;
+      //   console.log(`changed grid square to ${newGridX}, ${newGridY}`);
     }
   }
 }
